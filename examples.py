9531dc9001e205b10cc389adaabe3ae493a2dd88
Lana Oreoluwa Sharon <lana_oreoluwa@yahoo.com>
2021-05-14 20:21:26 +0100

    initial commit

---
+++ examples.py
@@ -0,0 +1,38 @@
+a = int(0.15)
+# print(type(a))
+# print(a)
+b = 25
+
+x = 'Hello!' # this is a string
+hello_string = "Hello world!"
+z  = """grfrtyufr guytfehrfbyegfir
+fkjvhfviuorlvkmnkl
+hvvukjfjvijkvngfkvnjlgfn"""
+
+g = tuple(['ore', 2, 'yes', 0.5])
+d = list(g)
+# print(dir(d))
+
+
+# print(type(g))
+# print(type(d))
+
+# print(g[0])
+# g[0] = 'Amilya'
+
+# print(g[0])
+
+# print(x[0:4])
+
+f = ('ore', 2, 'yes', 0.5)
+# f[0] = 'Amilya'
+print(f[0])
+
+
+# k = {'ore', 2, 'yes', 0.5}
+
+# print(k)
+
+# dict_v = {'name': 'ore','height': 53, 'class':3}
+# print(dict_v.keys())
+
